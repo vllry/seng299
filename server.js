d64dc9052601388d852068c168fd117d1912b89a
kennethhyk <kenneth@w134-87-139-252.wireless.uvic.ca>
2015-06-23 14:08:08 -0700

    database connection

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var app = require('./app'),
 	config = require('./application/models/config'); // get database, port info
 
 
-mongoose.connect(config.database, function(err) {
+/*mongoose.connect(config.database, function(err) {
 	if(err) {
 		console.log(err);
 	} else {
 		console.log('Connecting to the database ...');
 	}
-});
+});*/
 
 
 app.listen(config.port, function(err) {
@@ -23,7 +23,23 @@ app.listen(config.port, function(err) {
 	}
 });
 
-var Db = require('mongodb').Db,
+var uriUtil = require('mongodb-uri');
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }; 
+
+var mongodbUri = 'mongodb://admin:123123@ds047722.mongolab.com:47722/library_booking';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+mongoose.connect(mongooseUri, options);
+
+var db = mongoose.connection;
+db.open(function(err, db) {
+  var collection = db.collection("bookings");
+  // Insert a single document
+  collection.insert({hello:'world_no_safe'});
+ }
+
+/*var Db = require('mongodb').Db,
     MongoClient = require('mongodb').MongoClient,
     Server = require('mongodb').Server,
     ReplSetServers = require('mongodb').ReplSetServers,
@@ -35,7 +51,8 @@ var Db = require('mongodb').Db,
     //BSON = require('mongodb').pure().BSON,
     assert = require('assert');
 
-var db = new Db('test', new Server('localhost', 27017));
+var mongoclient = new MongoClient(new Server("ds047722.mongolab.com", 47722), {native_parser: true});
+var db = new Db('test', new Server('ds047722.mongolab.com', 47722));
 // Fetch a collection to insert document into
 db.open(function(err, db) {
   var collection = db.collection("bookings");
@@ -52,4 +69,4 @@ db.open(function(err, db) {
       db.close();
     })
   }, 100);
-});
+});*/
