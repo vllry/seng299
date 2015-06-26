5cbd4dd382edbd65362b50c435b39cb3347d404c
francesyu90 <francesyu90@yahoo.com>
2015-06-26 11:50:10 -0700

    update booking.js
    endTime -> duration, change of primary key

--- app/models/booking.js
+++ app/models/booking.js
@@ -7,13 +7,13 @@ var BookingSchema   = new Schema({
 	//id: { type: String, index: { unique: true }},
 	bookedBy: { type: String, required: true },
 	startTime: { type: Date, required: true },
-	endTime: Date,
+	duration: Number,
 	roomId: { type: String, required: true },
 	projectorId: Number,
 	laptopId: Number
 });
 
-BookingSchema.index({ bookedBy: 1, roomId: 1, startTime: 1}, { unique: true });
+BookingSchema.index({ roomId: 1, startTime: 1}, { unique: true });
 
 
 // return the model
