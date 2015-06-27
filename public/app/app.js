94de2a8ef0cb77d52bf447a4e2935a3ae2b3bc70
BoyangJ <boyang.jiao@gmail.com>
2015-06-27 14:42:34 -0700

    fixed some angular things

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,11 @@
-angular.module('app', [])
+angular.module('userApp', [])
+
+.controller('mainController', function(){
+
+	var vm = this;
+
+	// basic variable to display
+	vm.message ="Library Study Room Booking"
+
+	
+});
