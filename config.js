07ac2e04766ce8295315f8010768a32f1cb76bab
BoyangJ <boyang.jiao@gmail.com>
2015-06-30 14:31:16 -0700

    Changed to new database
    account name: seng299project
    account email: seng299project@yahoo.com
    account password: tGt-q3d-6LY-DkU

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'port': process.env.PORT || 3000, /*Can someone figure out how to change the port without crashing the app?*/
-	"database": "mongodb://admin:123123@ds047722.mongolab.com:47722/library_booking",
+	"database": "mongodb://admin:123123@ds031531.mongolab.com:31531/library_booking",
 	//Manual DB access (only use if you know what you're doing!) with
 	//mongo --host ds047722.mongolab.com --port 47722 -u admin -p 123123 library_booking
 	'secret': 'seng299lab7_secret'
