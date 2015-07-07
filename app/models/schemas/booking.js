75e577210837a04871ba595390924adfb1fba4d1
Vallery Lancey <vallerylancey@gmail.com>
2015-07-07 12:54:44 -0700

    Added some indices to the mongoose schemas

--- app/models/schemas/booking.js
+++ app/models/schemas/booking.js
@@ -4,7 +4,7 @@ var Schema       = mongoose.Schema;
 // booking schema 
 var BookingSchema   = new Schema({
 	//id: { type: String, index: { unique: true }},
-	bookedBy: { type: Schema.Types.ObjectId, ref: 'User', select: true },
+	bookedBy: { type: Schema.Types.ObjectId, ref: 'User', select: true, required: true}, //index: true
 	startTime: { type: Date, required: true },
 	duration: {type: Number, min: 1, max: 6}, //Represents length of time in half-hour blocks (IE, duration:3 is 1.5 hours)
 	roomid: { type: String, required: true },
@@ -12,7 +12,7 @@ var BookingSchema   = new Schema({
 	laptopid: Number
 });
 
-BookingSchema.index({ roomid: 1, startTime: 1}, { unique: true });
+BookingSchema.index({ roomid: 1, startTime: 1}, { unique: true }); 
 
 
 
