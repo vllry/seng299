33a65faf1d184e6fae9e832f92fb1aceb002d357
Vallery Lancey <vallerylancey@gmail.com>
2015-07-10 11:29:26 -0700

    Fixed misspelling of faculty in user schema (was preventing faculty members from being created)

--- app/models/schemas/user.js
+++ app/models/schemas/user.js
@@ -9,7 +9,7 @@ var UserSchema   = new Schema({
 	studentid: String, //Cannot require:true because not everyone is a student -> not everyone has a V-number
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
-	userType: { type: String, enum : ['student', 'staff', 'facaulty'], required: true },
+	userType: { type: String, enum : ['student', 'staff', 'faculty'], required: true },
 	department: String,
 	role: { type: String, enum : ['user', 'admin', 'superAdmin'], default : 'user', required: true},
 	bookingRestriction: Date
