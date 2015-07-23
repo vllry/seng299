b9f71161242518ee7f9cc1ccf1ecebf80a2b595e
Vallery Lancey <vallery@messages.xyz>
2015-07-22 21:19:59 -0700

    Much wow, very made-clicking-booking-cells-other-than-the-first-associate-with-the-booking

--- app/models/database-facade.js
+++ app/models/database-facade.js
@@ -83,6 +83,22 @@ function calculateEndTime(startTime, duration) {
 
 
 
+function getBookingAtTime(time, fn) {
+	schemaBooking.findOne({ //Finds all bookings in other rooms that overlap with this booking's time.
+		'startTime' : {$lt : bookingData['endTime']},
+		'endTime' : {$gt : bookingData['startTime']},
+		'roomid' : {$ne : bookingData['roomid']}
+		},
+		function(err, booking) {
+			console.log("Vallery's debug: ");
+			console.log(booking);
+			fn();
+		}
+	);
+}
+
+
+
 function handleEquipment(res, bookingData, requestedLaptop, requestedProjector, fn) {
 	if (requestedLaptop || requestedProjector) {
 		if ((requestedLaptop && !bookingData['laptop']) || (requestedProjector && !bookingData['projector'])) {
@@ -367,11 +383,15 @@ exports.bookingCreate = function(res, bookingData, requestedLaptop, requestedPro
 
 
 exports.bookingDelete = function(res, roomid, startTime, netlinkid) {
-	schemaBooking.findOne({'roomid' : roomid, 'startTime' : startTime}).populate('bookedBy', 'netlinkid').exec(function(err, data) {
+	schemaBooking.findOne({ //Assume startTime may be in the middle of the booking
+		'startTime' : {$lte : startTime},
+		'endTime' : {$gt : startTime},
+		'roomid' : roomid
+	}).populate('bookedBy', 'netlinkid').exec(function(err, data) {
 		if (data) {
-			schemaBooking.remove({'roomid' : roomid, 'startTime' : startTime}, function(err) {
+			schemaBooking.remove({'roomid' : roomid, 'startTime' : data['startTime']}, function(err) {
 				var now = new Date();
-				if (data['bookedBy']['netlinkid'] == netlinkid && (startTime - now) / 3600000 <= 5) {
+				if (data['bookedBy']['netlinkid'] == netlinkid && (data['startTime'] - now) / 3600000 <= 5) {
 					console.log(netlinkid + ' deleted a booking within 5 hours of the start');
 					userSetDateRestriction(netlinkid, now);
 				}
@@ -387,10 +407,15 @@ exports.bookingDelete = function(res, roomid, startTime, netlinkid) {
 
 
 exports.bookingUpdate = function(res, bookingData, requestedLaptop, requestedProjector) {
-	schemaBooking.findOne({'roomid' : bookingData['roomid'], 'startTime' : bookingData['startTime']}, function(err, data) {
+	schemaBooking.findOne({ //Assume startTime may be in the middle of the booking
+		'startTime' : {$lte : bookingData['startTime']},
+		'endTime' : {$gt : bookingData['startTime']},
+		'roomid' : bookingData['roomid']
+	}, function(err, data) {
 		if (data) {
 			bookingData['bookedBy'] = data['bookedBy'];
-			//console.log(bookingData);
+			bookingData['startTime'] = data['startTime']; //May be different
+			console.log(bookingData);
 			bookingValidate(bookingData, function(result) {
 				bookingData['endTime'] = calculateEndTime(bookingData['startTime'], bookingData['duration']);
 				//console.log(result);
@@ -413,6 +438,7 @@ exports.bookingUpdate = function(res, bookingData, requestedLaptop, requestedPro
 					});
 				}
 				else {
+					console.log(result);
 					res.json(result);
 				}
 			});
